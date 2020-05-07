9733231de0e7941e804e5bcff81ea341ead6f78d
Kimia <kimia_kimi77@yahoo.com>
2020-05-07 23:13:16 +0430

    register fragment layout completed

---
+++ app/src/main/java/ir/sbu/softwareengineering_proposal/ui/registerFragment/RegisterFragment.java
@@ -0,0 +1,29 @@
+package ir.sbu.softwareengineering_proposal.ui.registerFragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ir.sbu.softwareengineering_proposal.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisterFragment extends Fragment {
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
