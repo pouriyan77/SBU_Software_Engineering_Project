b4342a6d1fc78ae770980597e37e4442beaaaa9d
Kimia <kimia_kimi77@yahoo.com>
2020-05-08 19:43:18 +0430

    colors of the whole project are changed + constants.kt is edited

--- app/src/main/java/ir/sbu/softwareengineering_proposal/utils/Constants.kt
+++ app/src/main/java/ir/sbu/softwareengineering_proposal/utils/Constants.kt
@@ -7,15 +7,21 @@ const val BASE_URL = "https://warm-tundra-16580.herokuapp.com/api/"
 
 const val addUserStr = "ثبت نام کاربر جدید"
 const val editUserStr = "ویرایش اطلاعات کاربران"
+const val uploadProposalStr = "آپلود پروپوزال"
+const val judgeProposalStr = "داوری پروپوزال"
+const val setSupervisorStr = "تعیین استاد راهنما"
 
 val adminDeskItems = listOf<DeskItem>(
     DeskItem(addUserStr, R.mipmap.add_user_icon),
     DeskItem(editUserStr, R.mipmap.edit_user_icon)
 )
 
-//studentDeskItem will be added
-//profDeskItem will be added
-//png 128 px xhdpi
-//تعیین استاد راهنما
-//داوری پروپوزال
-//آپلود پروپوزال برای دانشجو
+val studentDeskItems = listOf<DeskItem>(
+    DeskItem(uploadProposalStr, R.mipmap.upload_proposal_icon)
+)
+
+val professorDeskItem = listOf<DeskItem>(
+    DeskItem(judgeProposalStr, R.mipmap.judge_icon),
+    DeskItem(setSupervisorStr, R.mipmap.set_supervisor_icon)
+)
+
