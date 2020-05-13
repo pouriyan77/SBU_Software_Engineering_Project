a026570d01ef0607bc3ee1243035fd0956aacb25
Kimia <kimia_kimi77@yahoo.com>
2020-05-13 18:03:09 +0430

    small changes

--- app/src/main/java/ir/sbu/softwareengineering_proposal/ui/usersListForAdminFragment/UsersListForAdmin.kt
+++ app/src/main/java/ir/sbu/softwareengineering_proposal/ui/usersListForAdminFragment/UsersListForAdmin.kt
@@ -1,13 +1,17 @@
 package ir.sbu.softwareengineering_proposal.ui.usersListForAdminFragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import ir.sbu.softwareengineering_proposal.R
+import ir.sbu.softwareengineering_proposal.adapter.MarginSpacingItemDecoration
 import ir.sbu.softwareengineering_proposal.adapter.RecyclerViewInteraction
+import ir.sbu.softwareengineering_proposal.adapter.usersRecycler.UsersRecyclerAdapter
+import ir.sbu.softwareengineering_proposal.utils.fakeUsers
+import kotlinx.android.synthetic.main.fragment_users_list_for_admin.*
 
-class UsersListForAdmin : Fragment(R.layout.fragment_users_list_for_admin),
+class UsersListForAdmin : androidx.fragment.app.Fragment(R.layout.fragment_users_list_for_admin),
     RecyclerViewInteraction {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -17,7 +21,11 @@ class UsersListForAdmin : Fragment(R.layout.fragment_users_list_for_admin),
 
 
     private fun setupRecyclerView() {
-
+        getUserRecyclerView?.apply {
+            adapter = UsersRecyclerAdapter(fakeUsers, this@UsersListForAdmin)
+            addItemDecoration(MarginSpacingItemDecoration(20))
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
     }
 
     override fun onItemClickedListener(position: Int) {
