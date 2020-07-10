995f3a0ed6739a48ad157333b171c480b3acd6d4
Kimia <kimia_kimi77@yahoo.com>
2020-07-10 17:54:35 +0430

    changes in uploadProposal layout

---
+++ app/src/main/java/ir/sbu/softwareengineering_proposal/ui/proposalStatusForStudentFragment/ProposalStatus.kt
@@ -0,0 +1,11 @@
+package ir.sbu.softwareengineering_proposal.ui.proposalStatusForStudentFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import ir.sbu.softwareengineering_proposal.R
+
+class ProposalStatus : Fragment
